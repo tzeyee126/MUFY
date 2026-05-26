78ce895f1fcc72572028047b22f6896599d02ed7
tzeyee126 <tzeyee0612@gmail.com>
2026-05-26 04:50:22 +0000

    Save project

---
+++ Part2/ex14.py
@@ -0,0 +1,11 @@
+# List of countries
+countries = ['Malaysia', 'Japan', 'Armenia', 'Brazil', 'Australia']
+
+# TODO:
+# Create a for loop that goes through the list and prints each country
+
+# Insert your code here
+countries = ['Malaysia', 'Japan', 'Armenia', 'Brazil', 'Australia']
+for x in countries:
+    print("Come and visit", x)
+    
---
+++ Part2/ex17.py
@@ -0,0 +1,3 @@
+name = input("What is your name?\n")
+print(f"{name}, your codename is: Fearless Dragon")
+print("Your lucky number is: 68")
